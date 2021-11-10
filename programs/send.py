ff1ad7e7ec9b919e5cec1b5a6c76e2d491611d82
Maksim Korzh <freesoft.for.people@gmail.com>
2021-11-10 16:01:23 +0200

    fixed pfixed python loader to output carriage return

--- programs/send.py
+++ programs/send.py
@@ -13,14 +13,21 @@ input()
 
 # wait until user clicks on putty window
 time.sleep(3)
-print('Sending bytes...')
+
+# paper tape
+paper = ''
 
 try:
     # open file and mimic typing
     with open(sys.argv[1]) as f:
-        for c in f.read():
-            print(c, end='')
-            pyautogui.press(c)
+        paper = f.read()
 
 except:
     print('Usage: python3 send.py path/to/file.ptf');
+
+# send butes
+print('Sending bytes...')
+for c in paper:
+    print(c, end='')
+    pyautogui.press(c)
+    if c == '\n': pyautogui.press('\r')
