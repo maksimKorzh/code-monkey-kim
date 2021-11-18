f802a074eb9374bec468ac3bd547bdc6e7b05cac
Maksim Korzh <freesoft.for.people@gmail.com>
2021-11-18 17:37:01 +0200

    improved paper tape upload logic

--- programs/send.py
+++ programs/send.py
@@ -1,33 +1,27 @@
 #
-# Mimics key presses on keyboard to send data via putty
+# Sends paper tape data to '/dev/ttyUSB0'
 # Usage: python3 send.py path/to/file.ptf
 #
 
-import pyautogui
 import time
 import sys
-
-print('1. Open your putty session')
-print('2. Press any key - you\'ll have 3 seconds to click on putty terminal window')
-input()
-
-# wait until user clicks on putty window
-time.sleep(3)
 
 # paper tape
 paper = ''
 
 try:
-    # open file and mimic typing
+    # read file
     with open(sys.argv[1]) as f:
         paper = f.read()
 
 except:
     print('Usage: python3 send.py path/to/file.ptf');
 
-# send butes
-print('Sending bytes...')
-for c in paper:
-    print(c, end='')
-    pyautogui.press(c)
-    if c == '\n': pyautogui.press('\r')
+# write to serial port
+with open('/dev/ttyUSB0', 'w') as f:
+    for c in paper:
+        print(c, end='')
+        f.write(c)
+        if c == '\n': f.write('\r')
+        time.sleep(0.004)
+        
